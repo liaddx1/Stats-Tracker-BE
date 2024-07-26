99dc939697d29af807044fb2673c237f64a60e3b
liaddx <liadd109@gmail.com>
2024-07-26 03:59:00 +0300

    Deleted the note bs Added new permission - only staff added two models, one for the initial info of the user and one for updating it's weight (needs to be a lot more properties) reverted settings code deleted note urls

--- statstrackerbe/api/models.py
+++ statstrackerbe/api/models.py
@@ -2,11 +2,64 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+class UserProfile(models.Model):
+    # profile_picture = models.ImageField(
+    #     upload_to="profile_pictures/", null=True, blank=True
+    # )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    full_name = models.CharField(max_length=30)
+    israeli_id = models.CharField(max_length=9, unique=True)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10)
+    city = models.CharField(max_length=10)
+    address = models.CharField(max_length=30)
+    height = models.CharField(max_length=10)
+    weight = models.CharField(max_length=10)
+    birthdate = models.DateField()
+    guardian_phone_number = models.CharField(max_length=10)
+    guardian_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+    is_having_regular_menstruation = models.BooleanField(default=True)
+    is_on_cocp = models.BooleanField(default=False)
+    # front_image = models.ImageField(upload_to="user_images/")
+    # side_image = models.ImageField(upload_to="user_images/")
+    # back_image = models.ImageField(upload_to="user_images/")
+    daily_routine = models.TextField()
+    daily_diet = models.TextField()
+    unliked_sauce = models.CharField(max_length=50)
+    alergies = models.CharField(max_length=50)
+    supplements = models.CharField(max_length=50)
+    liked_food = models.TextField()
+    num_of_meals = models.CharField(max_length=10)
+    is_drinking_soda = models.BooleanField(default=False)
+    which_soda = models.TextField()
+    liked_sauce = models.CharField(max_length=50)
+    is_able_to_weight_food = models.BooleanField(default=False)
+    is_drinking_alcohol = models.BooleanField(default=False)
+    which_alcohol = models.TextField()
+    was_under_thousand_calories_diet = models.BooleanField(default=False)
+    thousand_calorie_diet = models.TextField()
+    current_training = models.TextField()
+    weekly_aerobic = models.TextField()
+    unliked_exercises = models.TextField()
+    commit_training_number = models.CharField(max_length=10)
+    former_injuries = models.TextField()
+    further_info = models.TextField()
 
     def __str__(self):
-        return self.title
+        return self.user.username
+
+
+class UserHistory(models.Model):
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="weight_history"
+    )
+    weight = models.CharField(max_length=10)
+    recorded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-recorded_at"]
+
+    def __str__(self):
+        f"{self.user_profile.user.username} - {self.weight} kg on {self.recorded_at}"
--- statstrackerbe/statstrackerbe/settings.py
+++ statstrackerbe/statstrackerbe/settings.py
@@ -98,15 +98,22 @@ WSGI_APPLICATION = "statstrackerbe.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "USER": os.getenv("DB_USER"),
-        "NAME": os.getenv("DB_NAME"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "ATOMIC_REQUESTS": True,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#         "USER": os.getenv("DB_USER"),
+#         "NAME": os.getenv("DB_NAME"),
+#         "PASSWORD": os.getenv("DB_PWD"),
+#         "ATOMIC_REQUESTS": True,
+#     }
+# }
 
 
 # Password validation
