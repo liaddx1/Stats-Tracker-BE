243d737257dfc80e75fe82ba8fc2169ec7617e54
liaddx <liadd109@gmail.com>
2024-08-09 05:38:13 +0300

    no message

--- statstrackerbe/statstrackerbe/settings.py
+++ statstrackerbe/statstrackerbe/settings.py
@@ -121,24 +121,24 @@ WSGI_APPLICATION = "statstrackerbe.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#         "USER": os.getenv("DB_USER"),
-#         "NAME": os.getenv("DB_NAME"),
-#         "PASSWORD": os.getenv("DB_PWD"),
-#         "ATOMIC_REQUESTS": True,
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "USER": os.getenv("DB_USER"),
+        "NAME": os.getenv("DB_NAME"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "ATOMIC_REQUESTS": True,
+    }
+}
 
 
 # Password validation
